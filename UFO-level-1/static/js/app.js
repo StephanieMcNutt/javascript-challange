8e75db898323f7e469bccc787ed1e0d4504e40e7
StephanieMcNutt <stephie4321@gmail.com>
2021-06-14 08:14:03 -0400

    commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// Reference table
+var tbody = d3.select("tbody");
+
+// get data into table columns
+tableData.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+
